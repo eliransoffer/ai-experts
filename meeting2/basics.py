f5e84c7fecc8d549f23cae12d532fd10d6d8658d
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2024-12-26 18:54:28 +0200

    change

---
+++ meeting2/basics.py
@@ -0,0 +1,12 @@
+print("Hello")
+num = 4
+print(type(num))
+num = 5.6
+print(type(num))
+word = "Sun"
+print(type(word))
+
+print(num + 5)
+
+user_input = input("Insert your name: ")
+print(user_input)
