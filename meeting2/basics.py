6932f9162cf7ed7de0f78bfa54fa5cbc9db106ea
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2024-12-26 19:28:21 +0200

    change

--- meeting2/basics.py
+++ meeting2/basics.py
@@ -8,5 +8,44 @@ print(type(word))
 
 print(num + 5)
 
-user_input = input("Insert your name: ")
-print(user_input)
+name = input("Insert your name: ")
+print(name)
+
+year = int(input("Your birth year: "))
+print(year)
+print(type(year))
+
+y1 = '1987'
+y2 = 1987
+
+# creates multiple strings by adding new word each time - not efficient
+print(name + " you are " + str(2024 - year) + " years old")
+
+# efficient - does not create many strings
+print(name, "you are", 2024 - year, "years old", sep="_", end="\n\n")
+
+# creates one long string
+print(f"{name} you are {2024 - year} years old")
+
+s1 = name, "you are", 2024 - year, "years old"
+print(s1)
+
+sentence1 = "The sky is blue"
+sentence2 = 'The sky is blue'
+
+sentence3 = 'She said: "The sky is blue"'
+text = """
+Line 1
+Line 2
+"""
+print(text)
+
+text = '''
+Line 1
+Line 2
+'''
+
+text3 = f'''
+Line 1 {name}
+Line 2
+'''
---
+++ meeting2/basics2.py
@@ -0,0 +1,9 @@
+# + - / * ** % //
+
+# modulus - remainder
+print(5 % 2)
+# div - the whole part
+print(5 // 2)
+
+minutes = 103
+# 01:43
