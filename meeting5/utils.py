9d42116304e72f6689efbca9967dbb602a77e626
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2025-01-06 20:38:20 +0200

    meeting 5

--- meeting5/utils.py
+++ meeting5/utils.py
@@ -4,12 +4,13 @@ months = {
     "two": "feb",
 }
 
-for k in months:
-    print(k, months[k])
+# for k in months:
+#     print(k, months[k])
+#
+# for k, v in months.items():
+#     print(k, v)
+#
+# for k in months.items():
+#     print(k)
 
-for k, v in months.items():
-    print(k, v)
-
-for k in months.items():
-    print(k)
 
