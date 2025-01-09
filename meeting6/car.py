0afcfbed739e1285736c72244a246e21b158a0eb
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2025-01-09 17:52:11 +0200

    meeting 6

--- meeting6/car.py
+++ meeting6/car.py
@@ -5,14 +5,18 @@ word = "sun"
 def add_km(c, km):
     c["km"] += km
 
+
+
 class Car:
-    def __init__(self, manufacturer, model, color):
+    def __init__(self, manufacturer, model, color, tank_capacity=50):
         self.manufacturer = manufacturer
         self.model = model
         self.color = color
+        self.tank_capacity = tank_capacity
         self.km = 0
 
-    # reset
+
+
 
 car1 = Car("mazda", "3", "white")
 print(car1.color)
