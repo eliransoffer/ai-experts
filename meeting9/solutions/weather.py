a82bc567697cd69b11ac4581bad590958cd4ec11
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2025-01-20 19:51:05 +0200

    meeting9

--- meeting9/solutions/weather.py
+++ meeting9/solutions/weather.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 import requests
 
 API_KEY = "cabc6287eb069fc97eaae2dd9e9da023"
@@ -6,10 +8,16 @@ API_KEY = "cabc6287eb069fc97eaae2dd9e9da023"
 city = input("insert city: ")
 
 # Build the URL for the OpenWeatherMap API to get the city coordinates
-geo_url = f"http://api.openweathermap.org/geo/1.0/direct?appid={API_KEY}&q={city}"
+# geo_url = f"http://api.openweathermap.org/geo/1.0/direct?appid={API_KEY}&q={city}"
+geo_url = "http://api.openweathermap.org/geo/1.0/direct"
+params = {
+    "appid": API_KEY,
+    "q": city
+}
+
 
 # Send the request and get the response
-geo_response = requests.get(geo_url)
+geo_response = requests.get(geo_url, params)
 
 # Convert the response to JSON
 geo_data = geo_response.json()
@@ -19,10 +27,17 @@ lat = geo_data[0]['lat']
 long = geo_data[0]['lon']
 
 # Build the URL for the OpenWeatherMap API to get the weather data
-weather_url = f"https://api.openweathermap.org/data/2.5/weather?appid={API_KEY}&lat={lat}&lon={long}&units=metric"
+weather_url = f"https://api.openweathermap.org/data/2.5/weather"
+params = {
+    "appid": API_KEY,
+    "lat": lat,
+    "lon": long,
+    "units": "metric"
+
+}
 
 # Send the request and get the response
-weather_response = requests.get(weather_url)
+weather_response = requests.get(weather_url, params)
 
 # Print the weather data
-print(weather_response.json())
+pprint(weather_response.json())
